d25afda7a4aba1efa3c867ea7ecd9f21132ea64b
Nicole Buendia <nicole.buendia424@gmail.com>
2026-03-09 19:00:45 -0400

    cleaned up email hook, added some logic into terminal hook and ellipsis hook

--- src/hooks/useContactMeEmail.ts
+++ src/hooks/useContactMeEmail.ts
@@ -2,8 +2,6 @@ import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store";
 
-import { useToast } from "./useToast";
-
 import {
   setShowEmailSection,
   setCurrentContactIndex,
@@ -11,28 +9,20 @@ import {
   setYesEmailPrompts,
   YesEmailPrompts,
   NoEmailPrompts,
-  setShowEllipsis,
   SendEmailPrompts,
   setSendEmailPrompts,
 } from "@/features/ContactMe";
 
-import {
-  triggerNoEmailAction,
-  triggerResetEmailPrompt,
-  triggerYesEmailAction,
-} from "@/lib/utils";
-
+import { setShowEllipsis } from "@/features/Ellipsis";
 
 export function useContactMeEmail() {
   const dispatch = useDispatch();
-  const { handleToast } = useToast();
 
   const showEmailSection = useSelector((state: RootState) => state.contactMeSlice.showEmailSection);
   const currentContactIndex = useSelector((state: RootState) => state.contactMeSlice.currentContactIndex);
   const sendEmailPrompts = useSelector((state: RootState) => state.contactMeSlice.sendEmailPrompts);  
   const noEmailPrompts = useSelector((state: RootState) => state.contactMeSlice.noEmailPrompts);  
   const yesEmailPrompts = useSelector((state: RootState) => state.contactMeSlice.yesEmailPrompts);  
-  const showEllipsis = useSelector((state: RootState) => state.contactMeSlice.showEllipsis);
   const displayForm = useSelector((state: RootState) => state.emailFormSlice.displayForm);
 
   const handleContactStateReset = useCallback(() => {
@@ -64,34 +54,9 @@ export function useContactMeEmail() {
     dispatch(setSendEmailPrompts({ [key]: value }));
   }, [dispatch]);
 
-  const handleNoEmailPrompts = useCallback((key: keyof NoEmailPrompts, value: boolean) => {
-    dispatch(setNoEmailPrompts({ [key]: value }));
-  }, [dispatch]);
-
   const handleYesEmailPrompts = useCallback((key: keyof YesEmailPrompts, value: boolean) => {
     dispatch(setYesEmailPrompts({ [key]: value }));
   }, [dispatch]);
-
-  const handleShowEllipsis = useCallback((state: boolean) => {
-    dispatch(setShowEllipsis(state));
-  }, [dispatch]);
-
-  function handleContactCommand(cmd: string) {
-    if (!showEmailSection) return;
-
-    if (cmd === "n" || cmd === "N")
-      triggerNoEmailAction(yesEmailPrompts, handleSendEmailPrompts, handleNoEmailPrompts);
-
-    else if (cmd === "y" || cmd === "Y")
-      triggerYesEmailAction(noEmailPrompts, handleSendEmailPrompts, handleYesEmailPrompts);
-    
-    else if (cmd.startsWith("--reset") && noEmailPrompts.triggerNoEmail) 
-      triggerResetEmailPrompt(handleShowEllipsis, handleNoEmailPrompts, handleSendEmailPrompts);
-
-    // WHEN RUN RUNNING OTHER COMMANDS, IT TRIGGERS ERROR TOAST AFTER N OR Y HAS BEEN ENTERED
-    else
-      handleToast(`Unknown command was entered: ${cmd}`);
-  }
 
   const triggerBlurAction = () => {
     handleYesEmailPrompts("triggerEmailBlurAnimation", true);
@@ -114,11 +79,7 @@ export function useContactMeEmail() {
     sendEmailPrompts,
     noEmailPrompts,
     yesEmailPrompts,
-    showEllipsis,
-    handleNoEmailPrompts,
     handleYesEmailPrompts,
-    handleShowEllipsis,
-    handleContactCommand,
     triggerBlurAction,
     handleContactStateReset,
   };
