f68fa198069f027c9b4b187273a3ab87d5c7b8d3
nbuendia <nicole.buendia424@gmail.com>
2026-05-25 18:09:36 -0400

    deleted old hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,12 +2,10 @@ import { useAsciiScroll } from "./useAsciiScroll";
 import { useCommandBar } from "./useCommandBar";
 import { useEllipsis } from "./useEllipsis";
 import { useToast } from "./useToast";
-import { useWelcomeBack } from "./useWelcomeBack";
 
 export {
   useAsciiScroll,
   useCommandBar,
   useEllipsis,
   useToast,
-  useWelcomeBack,
 };
