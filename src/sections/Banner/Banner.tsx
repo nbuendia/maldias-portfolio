9176be58b4b624d11eebfc9b5b1af1193811d78a
nbuendia <nicole.buendia424@gmail.com>
2026-05-25 15:34:43 -0400

    clean up spacing

--- src/sections/Banner/Banner.tsx
+++ src/sections/Banner/Banner.tsx
@@ -7,17 +7,17 @@ import { Box } from "@/components/Box";
 import styles from "./Banner.module.css";
 
 export default function Banner() {
-    return (
-        <header className={styles.container}>
-            <Box column className={styles.subContainer}>
-                <h1 className={styles.name}>
-                    {FIRST_NAME} {LAST_NAME}
-                </h1>
+  return (
+    <header className={styles.container}>
+      <Box column className={styles.subContainer}>
+        <h1 className={styles.name}>
+          {FIRST_NAME} {LAST_NAME}
+        </h1>
                 
-                <h4 className={styles.bio}>
-                    I make websites that feel like magic.
-                </h4>
-            </Box>
-        </header>
-    )
+        <h4 className={styles.bio}>
+          I make websites that feel like magic.
+        </h4>
+      </Box>
+    </header>
+  )
 }
--- src/sections/Greeting/Greeting.tsx
+++ src/sections/Greeting/Greeting.tsx
@@ -9,50 +9,50 @@ import { Icon } from "@/components/Icon";
 import styles from "./Greeting.module.css";
 
 export default function Greeting() {
-    const {showComponent, endGreeting, handleEndAnimation} = useGreeting();
-    const firstName = FIRST_NAME;
-    const lastName = LAST_NAME;
-    const nameClasses = (letter: string) =>  `${styles.letter} ${letter === " " ? styles.space : ""}`;
+  const {showComponent, endGreeting, handleEndAnimation} = useGreeting();
+  const firstName = FIRST_NAME;
+  const lastName = LAST_NAME;
+  const nameClasses = (letter: string) =>  `${styles.letter} ${letter === " " ? styles.space : ""}`;
 
-    return (
-        <>
-            {showComponent && (
-                <Box column className={`${styles.center} ${endGreeting ? styles.end : ''}`}>
-                    <span className={styles.hola}>
-                        <h1>HOLA</h1> 
-                        <Icon name="waving_hand" size="32px" className={styles.wave} />
-                    </span>
+  return (
+    <>
+      {showComponent && (
+        <Box column className={`${styles.center} ${endGreeting ? styles.end : ''}`}>
+          <span className={styles.hola}>
+            <h1>HOLA</h1> 
+            <Icon name="waving_hand" size="32px" className={styles.wave} />
+          </span>
 
-                    <Box column>
-                        <span className={styles.soy}>
-                            <h3>I AM</h3>
-                        </span>
+          <Box column>
+            <span className={styles.soy}>
+              <h3>I AM</h3>
+            </span>
 
-                        <Box className={styles.me}>
-                            <h1 className={styles.name}>
-                                {[...firstName].map((letter, i) => (
-                                    <span key={i} className={nameClasses(letter)}
-                                        style={{animationDelay: `${i * 0.1 + 3}s`}}>
-                                        {letter}
-                                    </span>
-                                ))}
-                            </h1>
-                            <h1 className={styles.name}>
-                                {[...lastName].map((letter, i) => (
-                                    <span key={i} className={nameClasses(letter)}
-                                        style={{animationDelay: `${i * 0.1 + 4}s`}}>
-                                        {letter}
-                                    </span>
-                                ))}
-                            </h1>
-                        </Box>
+            <Box className={styles.me}>
+              <h1 className={styles.name}>
+                {[...firstName].map((letter, i) => (
+                  <span key={i} className={nameClasses(letter)}
+                    style={{animationDelay: `${i * 0.1 + 3}s`}}>
+                    {letter}
+                  </span>
+                ))}
+              </h1>
+              <h1 className={styles.name}>
+                {[...lastName].map((letter, i) => (
+                  <span key={i} className={nameClasses(letter)}
+                    style={{animationDelay: `${i * 0.1 + 4}s`}}>
+                    {letter}
+                  </span>
+                ))}
+              </h1>
+            </Box>
 
-                        <span className={styles.title} onAnimationEnd={() => handleEndAnimation()}>
-                            <h3>A FRONTEND DEVELOPER</h3>
-                        </span>
-                    </Box>
-                </Box>
-            )}
-        </>
-    )
+            <span className={styles.title} onAnimationEnd={() => handleEndAnimation()}>
+              <h3>A FRONTEND DEVELOPER</h3>
+            </span>
+          </Box>
+        </Box>
+      )}
+    </>
+  )
 }
