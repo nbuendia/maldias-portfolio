4d122e1b577024b4355fa1d756130dfbd9843910
nbuendia <nicole.buendia424@gmail.com>
2026-05-25 18:10:40 -0400

    updated varible name after changing it in redux

--- src/sections/Main/hooks/useMain.ts
+++ src/sections/Main/hooks/useMain.ts
@@ -9,12 +9,12 @@ import { useShowGreeting } from "@/sections/Greeting/hooks";
 export function useMain() {
   const dispatch = useDispatch();
   const showComponent = useSelector((state: RootState) => state.mainSlice.showComponent);
-  const showWelcomeComponent = useSelector((state: RootState) => state.welcomeBackSlice.showComponent);
+  const showWelcomeBack = useSelector((state: RootState) => state.welcomeBackSlice.showWelcomeBack);
   const {showGreeting} = useShowGreeting();
 
   useEffect(() => {
-    dispatch(setShowComponent(!showGreeting && !showWelcomeComponent));
-  }, [dispatch, showGreeting, showWelcomeComponent]);
+    dispatch(setShowComponent(!showGreeting && !showWelcomeBack));
+  }, [dispatch, showGreeting, showWelcomeBack]);
   
   return {
     showComponent,
