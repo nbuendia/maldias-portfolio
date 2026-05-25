3274bb4f007d8ac3a67f1ec883fefca3a6a26f88
nbuendia <nicole.buendia424@gmail.com>
2026-05-25 11:20:25 -0400

    deleted old hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,5 @@
 import { useAsciiScroll } from "./useAsciiScroll";
 import { useCommandBar } from "./useCommandBar";
-import { useConactMeInfo } from "./useConactMeInfo";
-import { useContactMeAscii } from "./useContactMeAscii";
-import { useContactMeEmail } from "./useContactMeEmail";
 import { useEllipsis } from "./useEllipsis";
 import { useEmailForm } from "./useEmailForm";
 import { useGreeting } from "./useGreeting";
@@ -12,9 +9,6 @@ import { useWelcomeBack } from "./useWelcomeBack";
 export {
   useAsciiScroll,
   useCommandBar,
-  useConactMeInfo,
-  useContactMeAscii,
-  useContactMeEmail,
   useEllipsis,
   useEmailForm,
   useGreeting,
