40d9346b8a63eafcf234087ec3ad23cb362034ea
nbuendia <nicole.buendia424@gmail.com>
2026-05-28 17:58:15 -0400

    used element refs to postion the tooltip left, below and above

--- src/components/Tooltip/hooks/useTooltip.ts
+++ src/components/Tooltip/hooks/useTooltip.ts
@@ -1,19 +1,30 @@
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useState } from "react";
 
-export function useTooltip() {
+export function useTooltip(
+  tooltipRef: RefObject<HTMLSpanElement | null>,
+  tooltipKidRefs: RefObject<HTMLSpanElement | null>,
+  position: string,
+) {
   const [isHovering, setIsHovering] = useState(false);
   const [exitHover, setExitHover] = useState(false);
   const [positionSpacing, setPositionSpacing] = useState<null | string>(null);
     
-  useEffect(() => {
-    const elem = document.getElementById("children");
-      
-    if (elem) {
-      const specs = elem?.getBoundingClientRect();
-      const left = specs?.left;
-      const width = specs?.width;
+  useEffect(() => {    
+    if (tooltipKidRefs?.current && tooltipRef?.current) {
+      const tooltipSpecs = tooltipRef.current.getBoundingClientRect();
+      const kidsSpecs = tooltipKidRefs.current.getBoundingClientRect();
+
+      const kidLeft = kidsSpecs.left;
+      const kidTop = kidsSpecs.top;
+      const kidWidth = kidsSpecs.width;
+      const kidHeight = kidsSpecs.height;
+
+      const tooltipHeight = tooltipSpecs.height;
   
-      setPositionSpacing(left + width + "px");      
+      if (position === "right") setPositionSpacing(kidLeft + kidWidth + "px");      
+      if (position === "left") setPositionSpacing(kidLeft - (kidWidth/2) + "px");  
+      if (position === "above") setPositionSpacing(kidTop - tooltipHeight - (kidHeight/2) + "px");  
+      if (position === "below") setPositionSpacing(kidTop + (kidHeight) + "px");        
     }
   });
 
