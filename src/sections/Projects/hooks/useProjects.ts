634aa93da8aed0c33d331cd0a29bae340e3509fd
nbuendia <nicole.buendia424@gmail.com>
2026-05-30 09:56:55 -0400

    updated hooks to remove values that arnt needed after deleting some features from the page. ASCII section is no longer there

--- src/sections/Projects/hooks/useProjects.ts
+++ src/sections/Projects/hooks/useProjects.ts
@@ -1,33 +1,18 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { 
-  setShowProjectsAscii,
   setShowProjectsSection,
-  setStartProjectAnimation,
-  setStartAsciiScrollAnim,
 } from "@/features/Projects";
 
 export function useProjects() {
   const dispatch = useDispatch();
 
-  const handleShowProjectAscii = useCallback(() => {
-    dispatch(setShowProjectsAscii(true));
-
-    const showProjectTimeout = setTimeout(() => {
-      dispatch(setShowProjectsSection(true));
-    }, 2000);
-
-    return () => clearTimeout(showProjectTimeout);
-  }, [dispatch]);
-
   useEffect(() => {
     const startProjectsAnimationTimeout = setTimeout(() => {
-      dispatch(setStartProjectAnimation(true));
+      dispatch(setShowProjectsSection(true));
     }, 1000);
 
     return () => clearTimeout(startProjectsAnimationTimeout);
   }, [dispatch]);
-
-  return { setStartAsciiScrollAnim, handleShowProjectAscii };
 }
--- src/sections/Projects/hooks/useProjectsReset.ts
+++ src/sections/Projects/hooks/useProjectsReset.ts
@@ -5,17 +5,13 @@ import {
   setCurrentProjectIndex,
   setProjects,
   setShowProjects,
-  setShowProjectsAscii,
   setShowProjectsSection,
-  setStartProjectAnimation,
 } from "@/features/Projects";
 
 export function useProjectsReset() {
   const dispatch = useDispatch();
 
   const handleProjectsStateReset = useCallback(() => {
-    dispatch(setShowProjectsAscii(false));
-    dispatch(setStartProjectAnimation(false));
     dispatch(setShowProjectsSection(false));
     dispatch(setProjects([]));
     dispatch(setShowProjects(false));
