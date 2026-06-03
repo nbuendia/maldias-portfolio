a42bd0d07a896732ac30c47e8aa9627fa99e25a6
nbuendia <nicole.buendia424@gmail.com>
2026-06-03 10:35:49 -0400

    added a new constats file inutils to replace the global consts file i deleted

---
+++ src/sections/Contact/utils/contactConstants.ts
@@ -0,0 +1,16 @@
+export const CONTACT_INFO_COMMAND = "cat contact-info.txt";
+export const CONTACT_INFO = [{
+  icon: "sentiment_excited",
+  label: "Name",
+  info: "Nicole Buendia"
+  }, {
+    icon: "code_xml",
+    label: "GitHub",
+    info: "https://github.com/nbuendia"
+  }, {
+    icon: "thumbs_up_double",
+    label: "LinkedIn",
+    info: "https://linkedin.com/in/nicole-buendia"
+  }];
+
+export const RESET_REGEX = /^--reset(?:\s+([\w-]+))?/;
--- src/sections/Contact/utils/index.ts
+++ src/sections/Contact/utils/index.ts
@@ -1,9 +1,13 @@
 import { triggerResetEmailPrompt } from "./resetEmail";
 import { triggerNoEmailAction } from "./noEmail";
 import { triggerYesEmailAction } from "./yesEmail";
+import { CONTACT_INFO, CONTACT_INFO_COMMAND, RESET_REGEX } from "./contactConstants";
 
 export {
   triggerResetEmailPrompt,
   triggerNoEmailAction,
   triggerYesEmailAction,
+  CONTACT_INFO_COMMAND,
+  CONTACT_INFO,
+  RESET_REGEX,
 };
