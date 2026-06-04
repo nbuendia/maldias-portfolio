f0dd90427c10e249dff5ebf59b6e98ac78d640cb
nbuendia <nicole.buendia424@gmail.com>
2026-06-04 13:07:20 -0400

    updated teminal componen to remove Box component and using a div element; added a children prop

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,22 +1,27 @@
-import { JSX } from "react";
-
-import { Box } from "@/components/Box";
+import { HTMLAttributes, JSX, ReactNode } from "react";
 
 import styles from "./Terminal.module.css";
 
-interface TerminalProps {
+interface TerminalProps extends HTMLAttributes<HTMLElement> {
   name: string;
   terminalComponentList: { [key: string]: JSX.Element };
+  children?: ReactNode;
 }
 
-export default function Terminal({ name, terminalComponentList }: TerminalProps) {
+export default function Terminal({ name, terminalComponentList, children, ...props }: TerminalProps) {
   const activeComponent = terminalComponentList[name as keyof typeof terminalComponentList];
   
+  const defaultClass = styles.container;
+  const incomingClasses = props.className && props.className;
+
+  const classes = [defaultClass, incomingClasses].filter(Boolean).join(" ");
+  
   return (
-    <Box className={styles.container}>
-      <Box column square className={styles.terminal}>
+    <div className={classes} style={props.style}>
+      <div className={styles.terminal}>
         {activeComponent}
-      </Box>
-    </Box>
+        {children && children}
+      </div>
+    </div>
   )
 }
