0ec47b4e0d432616b4faa10d40682f7599fd2465
nbuendia <nicole.buendia424@gmail.com>
2026-06-11 15:51:56 -0400

    added input value and setter as arguments to the hook so they can be properly passed and utilized in hook and functions

--- src/components/CommandBar/hooks/useCommandBar.ts
+++ src/components/CommandBar/hooks/useCommandBar.ts
@@ -7,9 +7,12 @@ import {
 } from "../actions";
 import { handleInsertChar } from "../utils";
 
-export function useCommandBar(onCommand: (cmd: string) => void) {
-  const [input, setInput] = useState('');
-  const [caretPosition, setCaretPosition] = useState(input.length)
+export function useCommandBar(
+  value: string,
+  onChange: (input: string) => void,
+  onCommand: (cmd: string) => void)
+  {
+  const [caretPosition, setCaretPosition] = useState(value.length)
 
   // MIGHT DELETE IN THE FUTURE
   const MAX_LENGTH = 250;
@@ -18,17 +21,16 @@ export function useCommandBar(onCommand: (cmd: string) => void) {
     event.preventDefault();
     const char = event.key.length === 1 ? event.key : '';
         
-    if (char && input.length < MAX_LENGTH) 
-      handleInsertChar(char, input, setInput, caretPosition, setCaretPosition);
+    if (char && value.length < MAX_LENGTH) 
+      handleInsertChar(char, value, onChange, caretPosition, setCaretPosition);
 
-    handleEnter(event, input, onCommand, setInput, setCaretPosition);
-    handleBackspace(event, input, setInput, caretPosition, setCaretPosition);
+    handleEnter(event, value, onCommand, onChange, setCaretPosition);
+    handleBackspace(event, value, onChange, caretPosition, setCaretPosition);
     handleArrowLeft(event, caretPosition, setCaretPosition);
-    handleArrowRight(event, input, caretPosition, setCaretPosition);
+    handleArrowRight(event, value, caretPosition, setCaretPosition);
   }
 
   return {
-    input,
     handleKeyDown,
     caretPosition,
   };
