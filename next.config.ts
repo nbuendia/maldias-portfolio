d6be628b3e7a0a6b684a53433209a770d0616d02
nbuendia <nicole.buendia424@gmail.com>
2026-06-11 21:43:18 -0400

    updated env variables for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    REACT_APP_S_ID: process.env.REACT_APP_S_ID,
-    REACT_APP_T_ID: process.env.REACT_APP_T_ID,
-    REACT_APP_P_KEY: process.env.REACT_APP_P_KEY,
-  },
+  env: {},
 };
 
 export default nextConfig;
--- src/lib/constants/emailFormConstants.ts
+++ src/lib/constants/emailFormConstants.ts
@@ -1,3 +1,3 @@
-export const S_ID = process.env.REACT_APP_S_ID;
-export const T_ID = process.env.REACT_APP_T_ID;
-export const P_KEY = process.env.REACT_APP_P_KEY;
+export const S_ID = process.env.NEXT_PUBLIC_S_ID;
+export const T_ID = process.env.NEXT_PUBLIC_T_ID;
+export const P_KEY = process.env.NEXT_PUBLIC_P_KEY;
