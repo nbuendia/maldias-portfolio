fb2104f8236c73d9c1f557e46b0ac2e7428c45a7
nbuendia <nicole.buendia424@gmail.com>
2026-08-20 20:10:37 -0400

    added gap styling for each contact object

--- src/sections/Contact/utils/contactConstants.ts
+++ src/sections/Contact/utils/contactConstants.ts
@@ -1,16 +1,19 @@
 export const CONTACT_INFO_COMMAND = "cat contact-info.txt";
 export const CONTACT_INFO = [{
-  icon: "sentiment_excited",
-  label: "Name",
-  info: "Nicole Buendia"
+    icon: "sentiment_excited",
+    label: "Name",
+    info: "Nicole Buendia",
+    gap: "50px",
   }, {
     icon: "code_xml",
     label: "GitHub",
-    info: "https://github.com/nbuendia"
+    info: "https://github.com/nbuendia",
+    gap: "30px",
   }, {
     icon: "thumbs_up_double",
     label: "LinkedIn",
-    info: "https://linkedin.com/in/nicole-buendia"
+    info: "https://linkedin.com/in/nicole-buendia",
+    gap: "10px",
   }];
 
 export const RESET_REGEX = /^--reset(?:\s+([\w-]+))?/;
