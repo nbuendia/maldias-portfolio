995f6aaa42422353f47712f44840a6ea880042da
nbuendia <nicole.buendia424@gmail.com>
2026-08-26 15:59:05 -0400

    moved text to a seperate file

--- src/sections/Home/utils/homeConstants.ts
+++ src/sections/Home/utils/homeConstants.ts
@@ -1,1 +1,9 @@
 export const TERMINAL_COMMANDS = ["home", "about", "projects", "contact"];
+
+export const INTRO = "Frontend focused software developer with experience building fullstack web applications, internal tools and intuitive user interfaces.";
+
+export const INSTRUCTIONS_PT_ONE = "This is a terminal style, interactive portfolio.";
+
+export const INSTRUCTIONS_PT_TWO = "Type commands in input below or "
+
+export const INSTRUCTIONS_PT_TWO_MOBILE = "use the dropdown on the upper right of the terminal to navigate through different parts of the site.";
