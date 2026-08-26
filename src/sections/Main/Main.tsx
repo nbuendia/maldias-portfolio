2b7be4333211ad58982c38d2050df56f8e670949
nbuendia <nicole.buendia424@gmail.com>
2026-08-26 14:22:23 -0400

    added mobile options prop to contact section component; resuing the function for the drop down button click

--- src/sections/Main/Main.tsx
+++ src/sections/Main/Main.tsx
@@ -32,7 +32,7 @@ export default function Main() {
     home: <Home />,
     about: <AboutMe />,
     projects: <Projects />,
-    contact: <Contact />
+    contact: <Contact handleMobileOptions={handleMenuItemClick} />
   };
 
   return (
