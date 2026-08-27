dee831c3666732c76c6ac526a5e303f6703bf70d
nbuendia <nicole.buendia424@gmail.com>
2026-08-27 09:46:44 -0400

    added animation to remove banner when user changes their mobile device to landscape to allow user to have more screen space to see the terminal

--- src/sections/Banner/Banner.tsx
+++ src/sections/Banner/Banner.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useDeviceCheck, useOrientation } from "@/hooks";
 import { FIRST_NAME, LAST_NAME } from "@/lib/constants";
 
 import { Box } from "@/components/Box";
@@ -7,8 +8,13 @@ import { Box } from "@/components/Box";
 import styles from "./Banner.module.css";
 
 export default function Banner() {
+  const {isMobileOrTablet} = useDeviceCheck();
+  const {orientation} = useOrientation();
+
+  const containerClasses = `${styles.container} ${orientation.orientation === "landscape" && isMobileOrTablet ? styles.orientation : ""}`;
+
   return (
-    <header className={styles.container}>
+    <header className={containerClasses}>
       <Box column className={styles.subContainer}>
         <h1 className={styles.name}>
           {FIRST_NAME} {LAST_NAME}
