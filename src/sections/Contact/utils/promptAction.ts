b81297363f43d365655988097a015297fe44d2e6
nbuendia <nicole.buendia424@gmail.com>
2026-08-27 15:37:45 -0400

    updated onclick to set styling to buttons to disable click actions

--- src/sections/Contact/utils/promptAction.ts
+++ src/sections/Contact/utils/promptAction.ts
@@ -1,15 +1,28 @@
 import { MouseEvent } from "react";
 
+function disableOnClick(e: MouseEvent) {
+  const elem = e.target as HTMLElement;
+  const buttonList = elem.parentElement && [...elem.parentElement.children];
+
+  buttonList?.forEach((button) => {
+    (button as HTMLElement).style.pointerEvents = "none";
+    (button as HTMLElement).style.opacity = "0.5";
+  });
+}
+
 export function triggerPromptAction(handleMobileOptions: (input: string) => void) {
   const handlePromptClick = (e: MouseEvent) => {
     const elem = e.target as HTMLElement;
-    const userResponse = elem.innerText.toLowerCase(); 
+    const userResponse = elem.innerText.toLowerCase();  
     
+    disableOnClick(e);
+
     if (userResponse === "yes") handleMobileOptions("y");
     if (userResponse === "no") handleMobileOptions("n");
   };
 
-  const handleResetClick = () => {
+  const handleResetClick = (e: MouseEvent) => {
+    disableOnClick(e)
     handleMobileOptions("--reset send-email");
   }
 
