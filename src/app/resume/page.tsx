a000473992b8785d805a36719a6e79f40db49480
nbuendia <nicole.buendia424@gmail.com>
2026-09-14 15:00:25 -0400

    deleted resume page to replace it with a get api route

--- src/app/resume/page.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./resume.module.css";
-
-export default function Resume() {
-  return (
-    <iframe src="/resumes/resume2025.pdf" title="Resume PDF" className={styles.pdfIframe}></iframe>
-  );
-}
